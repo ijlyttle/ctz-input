1ed28b0f6e1186f6595090f6fcf69712cbec8a64
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-12-05 14:27:52 -0600

    get webpack working (a little bit)

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,26 @@
 module.exports = {
-  mode: 'development',
-  entry: './dist/app.js',
+//  mode: 'development',
+  entry: './app/assets/js/app.ts',
+  devtool: 'inline-source-map', // helps debigging
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.app.json'
+            }
+          }
+        ],
+        exclude: /node-modules/
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js'
   }
