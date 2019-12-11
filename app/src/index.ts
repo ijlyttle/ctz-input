e89c734479b064646b19107b4fe6ed4527aac863
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-12-11 06:45:16 -0600

    get app working

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,9 @@
 import {CtzInput, CtzRadio, CtzSlider} from '../../src/index.js';
 
-const rad = new CtzRadio('foo');
+const rad00 = new CtzRadio('radio-00');
+
+rad00.description_text = 'Radio buttons';
+rad00.label_text = 'Description';
+rad00.init(['a', 'b'], 'b', false);
 
 console.log('yo');
